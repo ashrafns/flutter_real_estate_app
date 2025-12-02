a2d016dbd7190ada6268db9606bce33f4c37b20f
ashraf <>
2025-12-03 02:19:11 +0300

    Update: Fixed scroll behavior in home screen

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c4be5b51966e6fd11f60dfb692d0abec",
+const RESOURCES = {"flutter_bootstrap.js": "11a9806500854ec0917bdad39d7f8bd2",
 "version.json": "322dfa94430cc69aa45ce83495f46466",
 "index.html": "9ae6ef373aaa5f6b8e1ac514920309d7",
 "/": "9ae6ef373aaa5f6b8e1ac514920309d7",
-"main.dart.js": "33eb3fb9ce32f1abc806191f2e789a7e",
+"main.dart.js": "22a4c34a3b73c6d946e3d0804d7b9dc4",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
